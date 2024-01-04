80335841531a7c9577b01aa0f0b8228165e7582b
Kuba <maciazek.jakub@gmail.com>
2024-01-04 16:11:00 +0100

    juz działa

--- neo4J.py
+++ neo4J.py
@@ -1,6 +1,6 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=('neo4j', 'neo4j'))
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=('neo4j', 'kuba1234'))
 session = driver.session()
 query = 'CREATE (m:Miasto {nazwa:"Warszawa"});'
 session.run(query)
